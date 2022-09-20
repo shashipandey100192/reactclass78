1772e1fec50ee12e5ef79fe7f4c50236bacccec9
windows <windows@microsoft.com>
2022-09-20 19:41:38 +0530

    change page

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ function Abc()
     return (
     <div>
     <h1>sdf dshfjsf kumar   abc</h1><h2>jkk</h2>
+    <h1>welcome to react js</h1>
     </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import Abc from './Home';
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Abc />
-    <App />
+
   </React.StrictMode>
 );
 
