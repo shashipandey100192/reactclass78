11f341811c9139b709db6a469e41ab740ab3bf83
windows <windows@microsoft.com>
2022-09-29 19:57:21 +0530

    use of usestate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Mycontact()
 {
-    return <h1> this is contact page </h1>
+
+const [first, second] =useState('this is text')
+const [inputtext, uptexttext] =useState('text')
+const xyz = ()=>
+{
+    second('this is computer education');
+    uptexttext('color');
+}
+
+
+return(
+    <>
+        <h1>contact page {first}</h1>
+        <input type={inputtext} value={first} onClick={xyz} />
+    </>
+)
+
 }
 
 export default Mycontact
--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import 'font-awesome/css/font-awesome.css';
 // import Mynav from './components/Navbar';
 // import Myreactnav from './components/Navbar1';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Abc } from 'react-router-dom';
 import Myroute from './components/Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Abc>
   {/* <Myhome />
   <Myabout />
   <Mynav />
   <Myreactnav /> */}
   <Myroute />
-  </BrowserRouter>
+  </Abc>
 
   </React.StrictMode>
 );
