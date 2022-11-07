cd0bdfcebd32f02a07637842b3cf63a4f7b42bfd
windows <windows@microsoft.com>
2022-11-07 19:56:59 +0530

    card

--- src/Mydatafatch.js
+++ src/Mydatafatch.js
@@ -1,46 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 
 function Mydatafatch() {
 
-// useEffect(()=>{
-//    axios
-//    .get('https://jsonplaceholder.typicode.com/posts')
-//    .then((res)=>{
-//     console.log(res.data);
-//    })
-
-// });
-
-const [stvariabl, stfunction] =useState([]);
-const abc = ()=>
-{
+  const [stvariabl, stfunction] = useState([]);
+  const abc = () => {
     axios
-    .get('https://jsonplaceholder.typicode.com/comments')
-    .then((res)=>{
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .then((res) => {
         stfunction(res.data);
-    })
-    
-}
+      })
+
+  }
 
 
   return (
     <div>
-    <div>Mydatafatch</div>
-    <button type='button' onClick={abc}> get data</button>
-   {/* {JSON.stringify(stvariabl)} */}
+      <div>Mydatafatch</div>
+      <button type='button' onClick={abc}> get data</button>
+      {/* {JSON.stringify(stvariabl)} */}
+
+
+      <div className='container'>
+        <div className='row'>
+          {stvariabl.map((x) => {
+            return (
+              <>
+
+                <div className='col-4 mt-3'>
+                  <div className="card">
+                    <div className="card-header">
+                      Featured key={x.id}
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{x.name}</h5>
+                      <p className="card-text" >{x.body}</p>
+                      <a href="#" className="btn btn-primary">{x.email}</a>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
 
 
 
-   {stvariabl.map((x)=>{
-      return <h1>{x.email}</h1>
-})}
-
-  
-
+        </div>
+      </div>
 
     </div>
+
   )
 }
 
